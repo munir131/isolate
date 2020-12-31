ad39cc4d0fbb577fb545910095c9da5ef8fc9a1a
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2020-12-31 01:19:59 +0100

    Always set the stack limit.

--- isolate.c
+++ isolate.c
@@ -655,7 +655,7 @@ setup_rlimits(void)
 
   RLIM(FSIZE, (rlim_t)fsize_limit * 1024);
 
-  RLIM(STACK, (stack_limit ? (rlim_t)stack_limit * 1024 : RLIM_INFINITY));
+  RLIM(STACK, (rlim_t)stack_limit * 1024);
   RLIM(NOFILE, 512);
   RLIM(MEMLOCK, 0);
 
